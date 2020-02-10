124d9ee0d9292fcc5c3a1a6d38f8c2a36e4235a5
mu-majid <muhammed.m@trufla.com>
2020-02-10 17:58:45 +0200

    Adding Joi validation for all api routes.

---
+++ ApiGateWay/src/api/auth/middleware/RequestValidator.js
@@ -0,0 +1,19 @@
+const Joi = require('joi');
+const middleware = (schema) => {
+  return (req, res, next) => {
+    const { error } = Joi.validate(req, schema);
+    const valid = error == null;
+
+    if (valid) {
+      return next();
+    }
+    else {
+      const { details } = error;
+      const message = details.map(i => i.message).join(',');
+
+      console.log('Request Validation Error: ', message);
+      return res.status(422).json({ error: message })
+    }
+  }
+}
+module.exports = middleware;
